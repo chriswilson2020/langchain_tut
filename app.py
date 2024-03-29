189d8888a15dca6cc8f7004577aebc7799de90fd
chriswilson2020 <73828727+chriswilson2020@users.noreply.github.com>
2024-03-29 11:15:47 +0100

    Update app.py
    Implemented functions to make it easier just comment out what you don't want

--- app.py
+++ app.py
@@ -41,10 +41,11 @@ from langchain_openai import ChatOpenAI
 # set up our llm with a nice cheap model for testing
 llm = ChatOpenAI(temperature=0.9, model_name="gpt-3.5-turbo")
 
-# Simple no chain model. Just to get used to making the OpenAI calls
-text = "What are 5 vacation destinations for someone who likes to eat pasta?"
-# output = llm.invoke(text)
-# print(output.content)
+def simple():
+	# Simple no chain model. Just to get used to making the OpenAI calls
+	text = "What are 5 vacation destinations for someone who likes to eat pasta?"
+	output = llm.invoke(text)
+	print(output.content)
 
 # Now we add in prompt templates in order to start making dynamic requests
 from langchain.prompts import PromptTemplate
@@ -54,24 +55,23 @@ prompt = PromptTemplate(
     template="What are 5 vacation destinations for someone who likes to eat {food}?",
 )
 
-# In this example the output of invoking the llm is a class so we access the content of
-# it through dot notation
+def usingPrompts():
+	# In this example the output of invoking the llm is a class so we access the content of
+	# it through dot notation
 
-# print(prompt.format(food='dessert'),"\n")
-
-# output = llm.invoke(prompt.format(food='dessert'))
-# print(output.content)
+	print(prompt.format(food='dessert'),"\n")
+	output = llm.invoke(prompt.format(food='dessert'))
+	print(output.content)
 
 
 # Lets make our first chain
 from langchain.chains import LLMChain
 
-# we will reuse the llm and prompt variables from above
+def firstChain():
+	# we will reuse the llm and prompt variables from above
 
-chain = LLMChain(llm=llm, prompt=prompt)
-
-#print(chain.invoke("fruit")["text"])
-
+	chain = LLMChain(llm=llm, prompt=prompt)
+	print(chain.invoke("fruit")["text"])
 
 # Now lets plug agents into the mix and access google search api
 
@@ -82,38 +82,51 @@ from langchain.agents import AgentExecutor
 from langchain_core.tools import Tool
 from langchain import hub
 
-# we will keep the llm model from above
+def firstAgent():
+	# we will keep the llm model from above
 
-# Load in some tools to use
-tools = load_tools(["serpapi", "llm-math"], llm=llm)
+	# Load in some tools to use
+	tools = load_tools(["serpapi", "llm-math"], llm=llm)
 
-# Now initialise the agent with :
-# 1. The tools
-# 2. The language model
-# 3. The type of agent we will use
+	# Now initialise the agent with :
+	# 1. The tools
+	# 2. The language model
+	# 3. The type of agent we will use
 
-# depreciated use more recent version
-#agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True)
+	# depreciated use more recent versions
+	#agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True)
 
-react_prompt = hub.pull("hwchase17/react")
-agent = create_react_agent(tools=tools, llm=llm, prompt=react_prompt)
-agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+	react_prompt = hub.pull("hwchase17/react")
+	agent = create_react_agent(tools=tools, llm=llm, prompt=react_prompt)
+	agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 
-#agent_executor.invoke(
-#	input={"input": "Who is the current leader of Japan? What is the largest prime number that is smaller than their current age? (make sure you convert the age to an
-# 	int before doing the calculation)"}
-#)
+	agent_executor.invoke(
+		input={"input": "Who is the current leader of Japan? What is the largest prime number that is smaller than their current age? (make sure you convert the age to an int before doing the calculation)"}
+	)
 
 from langchain.chains import ConversationChain
 from langchain.memory import ConversationBufferMemory
 
-# We will reuse our llm as before
+def convoMemory():
+	# We will reuse our llm as before
 
-conversation = ConversationChain(llm=llm, memory=ConversationBufferMemory())
+	conversation = ConversationChain(llm=llm, memory=ConversationBufferMemory())
 
-conversation.predict(input="Hi there!")
-conversation.predict(input="We are having a conversation together")
-conversation.predict(input="The weather is nice today")
-conversation.predict(input="What is the first thing that I said to you?")
+	conversation.predict(input="Hi there!")
+	conversation.predict(input="We are having a conversation together")
+	conversation.predict(input="The weather is nice today")
+	conversation.predict(input="What is the first thing that I said to you?")
 
-print(conversation.memory.buffer)
+	print(conversation.memory.buffer)
+	
+	
+	
+simple()
+print("\n")
+usingPrompts()
+print("\n")
+firstChain()
+print("\n")
+firstAgent()
+print("\n")
+convoMemory()
